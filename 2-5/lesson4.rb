02b07828861371f6149af199a7a37e69838e3e29
にしゆうき <nishiyuki@nishiyuukinoMacBook-Air.local>
2019-09-03 15:48:08 +0900

    [Finish]2-2...9

---
+++ 2-5/lesson4.rb
@@ -0,0 +1,2 @@
+name = "Yuki Nishi"
+puts name
---
+++ 2-6/lesson5.rb
@@ -0,0 +1,30 @@
+# 配列オブジェクトは繰り返し文と併用することが多いです。
+# 複数の値を連続で取り出す処理を簡潔に書くことができます。
+
+subjects = ["国語","算数","理科","社会"]
+puts subjects[2]
+
+
+
+
+
+
+
+
+
+# これは間違ってる。
+# ハッシュ
+# 多分、値が数字ではないから？
+# subjects = {"Japanese"=>国語,"Math"=>算数,"Sciense"=>理科,"Social"=>社会}
+# puts subjects[Sciense]
+
+
+
+
+
+
+
+
+# シンボル
+# subjects = {:Japanese=>国語,:Math=>算数,:Sciense=>理科,:Social=>社会}
+# puts subjects[Sciense]
